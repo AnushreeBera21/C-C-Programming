cc75ac729af9372c836576dfcca8b1ce1e6abc5f
Sourav Bera <sauravberaiit@gmail.com>
2022-10-04 09:48:36 +0530

    Create hasPathSum

---
+++ hasPathSum.c
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) return false;
+        
+        if(root.left == null && root.right == null){
+            if(root.val == sum) return true;
+            return false;
+        }
+        
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
